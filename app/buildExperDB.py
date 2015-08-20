7a5a2cf0881fdce420eced527c6e8b63d9771656
Frank Bolton <frankjbolton@gmail.com>
2015-08-20 12:04:56 +0300

    edited the buildexp script

--- app/buildExperDB.py
+++ app/buildExperDB.py
@@ -4,12 +4,13 @@ import os.path, time, random
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-dbExperimentParameters = TinyDB(os.path.join(basedir,'dbExperimentParameters.json'))
-ParametersTable = dbExperimentParameters.table('Parameters')
-UserTracking = dbExperimentParameters.table('TrackUsers')
+#dbExperimentParameters = TinyDB(os.path.join(basedir,'dbExperimentParameters.json'))
+#ParametersTable = dbExperimentParameters.table('Parameters')
+#UserTracking = dbExperimentParameters.table('TrackUsers')
 
 dbExperimentParameters = TinyDB(os.path.join(basedir,'dbExperimentParameters.json'))
 ParametersTable = dbExperimentParameters.table('Parameters')
+UserTracking = dbExperimentParameters.table('TrackUsers')
 
 #start with a single group with a single subject.
 ParametersTable.insert({groupID: 1})
