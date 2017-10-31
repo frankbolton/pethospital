338113b2cbac5c600b51bcfae6eec54978fe8e4e
Frank Bolton <frankjbolton@gmail.com>
2017-10-31 16:25:02 +0200

    removed all of the old files to make the re-write cleaner

--- app/static/scripts/controllers/null.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('ktz')
-  .controller('nullCtrl', function ($scope) {
-  });
---
+++ pethospital.js
@@ -0,0 +1,6 @@
+var pets = document.getElementsByClassName("pets");
+
+for (i=0; i<pets.length; i++){
+    var x = pets[i];
+    x.innerHTML = "this is a good thingvagsdfgdsfgsd "+i;
+}
