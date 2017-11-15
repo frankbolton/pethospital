018412ac9273f0b7443155817af026ee6789ec4c
Frank Bolton <frankjbolton@gmail.com>
2017-11-15 06:45:23 +0200

    some changes to the refactor branch

--- host.py
+++ host.py
@@ -2,20 +2,31 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__, static_url_path='/static')
 
 
-#to run FLASK_APP=host.py flask run
+#to run on linux
+# activate venv:    source venv/bin/activate
+# add program to the environment variable and then run
+#  FLASK_APP=host.py flask run
+#
+#To run in windows 10:
+# activate venv:    venv/scripts/activate
+# set up environment variable:  set FLASK_APP=host.py
+# optional debug:               set FLASK_DEBUG=1
+# run the application           flask.run
+
+
 
 
 @app.route("/")
 def hello():
-    return "hello world"
-    
+    #return "hello world"
+    return render_template("uploadTest.html")
 
 @app.route("/experiment", methods=['GET', 'POST'])
 def experiment():
     if request.method == 'POST':
         print('inside post')
         #do some post action.... save the logs to the database and continue with experiment flow
-        print request.form['logs']
+        print(request.form['logs'])
         return redirect('/')
 
     else:
